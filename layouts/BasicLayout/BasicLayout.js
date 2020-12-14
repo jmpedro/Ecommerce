b784c58a2f9f83d061b8b8200510ca7d60cf842f
jmpedro <pedrojesusjimenezmartinez@gmail.com>
2020-12-15 00:41:00 +0100

    project structure initializated

---
+++ layouts/BasicLayout/BasicLayout.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container } from 'semantic-ui-react'
+
+export default function BasicLayout(props) {
+
+    const { children } = props;
+
+    return (
+        <Container fluid className="basic-layout">
+            <Container className="content">
+
+                {children}
+
+            </Container>
+        </Container>
+    )
+}
