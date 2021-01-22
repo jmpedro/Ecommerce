405bfd6aa45e633c821428ef0cd27a2fd82d303c
jmpedro <pedrojesusjimenezmartinez@gmail.com>
2021-01-22 13:16:17 +0100

    application finished

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const BASE_PATH = "http://localhost:1337/";
+export const BASE_PATH = "https://ecommerce-game-nextjs.herokuapp.com/";
 export const TOKEN = "token";
 export const CART = "cart";
 export const STRIPE_TOKEN = "pk_test_51IBmkyDbg7dVZ5O92v7u9ScST50mlEf1Hht4eDeIm4YtVoWXjkjTwG9lrEr3QdgXAD37eZMjHDrL243dUdoqGImI00TrOUtjX4";
